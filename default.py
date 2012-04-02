07216d9b1d51b4e1df39d7a0986df18114510121
dethfeet <dethfeet@qip.ru>
2012-04-02 14:54:28 +0200

    FIX: plugin.video.cinemassacre updating spike.com and blip.tv

--- default.py
+++ default.py
@@ -25,9 +25,9 @@ _regex_extractEpisodeImg2 = re.compile("<img width=\"[0-9]*\" height=\"[0-9]*\" 
 
 #Bip.tv
 #_regex_extractVideoBip = re.compile("<!-- video -->.*?<iframe src=\"(.*?)\".*?allowfullscreen>.*?<!-- /video -->",re.DOTALL);
-_regex_extractVideoBip = re.compile("<!-- video -->.*?(http://blip.tv/play/.*?)\".*?<!-- /video -->", re.DOTALL);
-_regex_extractVideoFeedURL = re.compile("&file=(.*?)&", re.DOTALL);
-_regex_extractVideoFeedURL2 = re.compile("&file=(.*)", re.DOTALL);
+_regex_extractVideoBip = re.compile("(<!-- video -->|<!-- content -->).*?(http://blip.tv/play/.*?)\".*?(<!-- /video -->|<!-- comments -->)", re.DOTALL);
+_regex_extractVideoFeedURL = re.compile("file=(.*?)&", re.DOTALL);
+_regex_extractVideoFeedURL2 = re.compile("file=(.*)", re.DOTALL);
 
 #_regex_extractFilesFromFeed = re.compile("<media:group>.*?<media:content url=\"(.*?)\".*?type=\"video/quicktime\".*?<media:content url=\"(.*?)\".*?type=\"video/x-m4v\".*?<media:content url=\"(.*?)\".*?type=\"video/x-flv\"",re.DOTALL);
 _regex_extractFilesFromFeedMov = re.compile("<media:content url=\"(.*?)\".*?type=\"video/quicktime\".*?");
@@ -54,8 +54,8 @@ _regex_extractVideoSpringboard = re.compile("<!-- (video|content) -->.*?http://(
 _regex_extractVideoSpringboardStream = re.compile("<media:content duration=\"[0-9]*?\" medium=\"video\" bitrate=\"[0-9]*?\" fileSize=\"[0-9]*?\" url=\"(.*?)\" type=\".*?\" />");
 
 #Spike.com
-_regex_extractVideoSpike = re.compile("<!-- video -->.*?<a href=\"(http://www.spike.com/.*?)\" target=\"_blank\">.*?<!-- /video -->", re.DOTALL);
-_regex_extraxtVideoSpikeId = re.compile("<meta property=\"og:video\" content=\"http://media.mtvnservices.com/mgid:arc:video:spike.com:(.*?)\" />");
+_regex_extractVideoSpike = re.compile("<!-- video -->.*?<a href=\"(http://www.spike.com/.*?)\".*?<!-- /video -->", re.DOTALL);
+_regex_extraxtVideoSpikeId = re.compile("<meta property=\"og:video\" content=\"(http://media.mtvnservices.com/mgid:arc:video:spike.com:(.*?))\" />");
 _regex_extractVideoSpikeSreamURL = re.compile("<rendition bitrate=\"(.*?)\".*?<src>(.*?)</src>.*?</rendition>",re.DOTALL)
 
 def mainPage():
@@ -139,7 +139,7 @@ def extractEpisodes(show):
     xbmcplugin.endOfDirectory(thisPlugin)
 
 def showEpisodeBip(videoItem):
-    url = videoItem.group(1)
+    url = videoItem.group(2)
     
     #GET the 301 redirect URL
     req = urllib2.Request(url)
@@ -227,7 +227,13 @@ def showEpisodeSpringboard(videoItem):
 def showEpisodeSpike(videoItem):
     videoUrl = videoItem.group(1)
     videoPage = load_page(videoUrl)
-    videoId = _regex_extraxtVideoSpikeId.search(videoPage).group(1)
+    swfUrl = _regex_extraxtVideoSpikeId.search(videoPage).group(1)
+    #GET the 301 redirect URL
+    req = urllib2.Request(swfUrl)
+    response = urllib2.urlopen(req)
+    swfUrl = response.geturl()
+    
+    videoId = _regex_extraxtVideoSpikeId.search(videoPage).group(2)
     feedUrl = "http://udat.mtvnservices.com/service1/dispatch.htm?feed=mediagen_arc_feed&account=spike.com&mgid=mgid%3Aarc%3Acontent%3Aspike.com%3A"+videoId+"&site=spike.com&segment=0&mgidOfMrssFeed=mgid%3Aarc%3Acontent%3Aspike.com%3A"+videoId
     videoFeed = load_page(feedUrl)
     videoStreamUrls = _regex_extractVideoSpikeSreamURL.finditer(videoFeed)
@@ -238,12 +244,13 @@ def showEpisodeSpike(videoItem):
         streamUrl = stream.group(2)
         streamBitrate = int(stream.group(1))
         if streamBitrate>curBitrate:
-            curStream = streamUrl
+            curStream = streamUrl.replace(" ","%20")
             curBitrate = streamBitrate
     
+    url = curStream + " swfUrl="+swfUrl+" swfVfy=1"
     if curStream is not None:
-        item = xbmcgui.ListItem(curStream)
-        xbmc.Player(xbmc.PLAYER_CORE_DVDPLAYER).play(curStream, item)
+        item = xbmcgui.ListItem(url)
+        xbmc.Player(xbmc.PLAYER_CORE_DVDPLAYER).play(url, item)
 
 def showEpisode(link):
     link = urllib.unquote(link)
